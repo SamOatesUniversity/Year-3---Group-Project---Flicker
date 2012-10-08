6e483ff6e34e1693ef46c194322b9cba65ca5064
Sam Oates <sam@samoatesgames.com>
2012-10-08 20:53:18 +0000

    Moved stuff from my branch (which wasn't really a branch on hind sight).
    It has basic player movement in it.

---
+++ DEV/Assets/Scripts/CCamera.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class CCamera : MonoBehaviour {
+	
+	/* -----------------
+	    Private Members 
+	   ----------------- */
+
+
+	/* ----------------
+	    Public Members 
+	   ---------------- */
+	
+	public int					DistanceFromPlayer = 10;		//!< How far away from the player should the camera be
+
+	/*
+	 * \brief Called when the object is created. At the start.
+	 *        Only called once per instaniation.
+	*/
+	public void Start () {
+		
+	}
+	
+	/*
+	 * \brief Called once per frame
+	*/
+	public void Update () {
+	
+	}
+}
---
+++ DEV/Assets/Scripts/CEntityPlayerBase.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class CEntityPlayerBase : MonoBehaviour {
+	
+	/* -------------------
+	    Protected Members 
+	   ------------------- */
+
+	protected Vector3		m_position;				//!< The current position of the entity
+	
+	protected string		m_name;					//!< The name of the entity (used on guis)
+	
+	/* ----------------
+	    Public Members 
+	   ---------------- */
+	
+	public int				Health = 100;			//!< The health of the entity
+
+	/*
+	 * \brief Called when the object is created. At the start.
+	 *        Only called once per instaniation.
+	*/
+	public virtual void Start () {
+		m_name = null;
+		m_position = transform.position;		
+	}
+	
+	/*
+	 * \brief Called once per frame
+	*/
+	public virtual void Update () {
+	
+		transform.position = m_position;
+		
+		Debug.Log(name + ": " + transform.position);
+		
+	}
+}
