76079a0f4a5712c3c2cdf94bd8fb5e519c24c897
Sam Oates <sam@samoatesgames.com>
2012-12-05 10:49:14 +0000

    Started moving logic to checkpoints

--- Flicker/Assets/Assets/Scripts/Triggers/CCheckPoint.cs
+++ Flicker/Assets/Assets/Scripts/Triggers/CCheckPoint.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 using System.Collections;
 
 public class CCheckPoint : MonoBehaviour {
+
+	private float m_playerAlpha = 0.0f;
+
+	void OnTriggerEnter(Collider collision) {
+
+		/*
+		CEnitityPlayer player = collision.other.gameobject.getComponent<CEntityPlayer>();
+		if (player != null)
+		{
+			player.LastCheckPoint = this;
+			m_playerAlpha = player.CurrentPlayerAlpha;
+		}
+		*/		
+		
+	}
+	
+	/*
+	*	\brief Get the player alpha stored at the checkpoint
+	*/
+	float PlayerCheckPointAlpha {
+		get {
+			return m_playerAlpha;
+		}
+	}
 		
 }
