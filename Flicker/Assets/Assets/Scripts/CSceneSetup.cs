e448dfdd3f0c2adbb70fce291904f0b216a43f69
Sam Oates <sam@samoatesgames.com>
2013-03-14 17:04:19 +0000

    Load in previous levels when loading a level.

--- Flicker/Assets/Assets/Scripts/CSceneSetup.cs
+++ Flicker/Assets/Assets/Scripts/CSceneSetup.cs
@@ -9,14 +9,56 @@ public class CSceneSetup : MonoBehaviour {
 	public string                       ThisScene = null;                       //! The name of this scene
     public string                       NextScene = null;                       //! The next scene to stream in
 	
+	public bool							RequiresDummyLevel = false;
+	
 	public Texture 						LoadingScreenTexture = null;
 	private bool						m_showLoadingScreen = true;
 	private float						m_loadCompleteTime = 0.0f;
 	
 	// Use this for initialization
     void Start() {
-				
+												
+        if ((NextScene == null || NextScene.Length == 0) && (LastScene == null || LastScene.Length == 0))
+        {
+			m_showLoadingScreen = false;
+            return;
+        }
+		
 		GameObject[] allLevels = GameObject.FindGameObjectsWithTag("Level");
+		
+		if (!(NextScene == null || NextScene.Length == 0)) {
+			
+			bool alreadyLoaded = false;
+			for (int levelIndex = 0; levelIndex < allLevels.Length; ++levelIndex)
+			{
+				if (allLevels[levelIndex].name == NextScene)
+				{
+					alreadyLoaded = true;
+					break;
+				}
+			}
+			
+			if (!alreadyLoaded) {
+				Application.LoadLevelAdditive(NextScene);
+			}
+		}
+		
+		if (!(LastScene == null || LastScene.Length == 0)) {
+			
+			bool alreadyLoaded = false;
+			for (int levelIndex = 0; levelIndex < allLevels.Length; ++levelIndex)
+			{
+				if (allLevels[levelIndex].name == LastScene)
+				{
+					alreadyLoaded = true;
+					break;
+				}
+			}
+			
+			if (!alreadyLoaded) {
+				Application.LoadLevelAdditive(LastScene);
+			}
+		}
 		
 		if (allLevels.Length > 1)
 		{
@@ -27,14 +69,6 @@ public class CSceneSetup : MonoBehaviour {
 				playerXform.gameObject.SetActiveRecursively(false);
 			}
 		}
-						
-        if (NextScene == null || NextScene.Length == 0)
-        {
-			m_showLoadingScreen = false;
-            return;
-        }
-		
-		Application.LoadLevelAdditive(NextScene);
     }
 	
 	void OnGUI() {
@@ -49,7 +83,7 @@ public class CSceneSetup : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (NextScene == null || NextScene.Length == 0)
+		if ((NextScene == null || NextScene.Length == 0) && (LastScene == null || LastScene.Length == 0))
 			return;
 		
 		if (m_hasSetup) {
